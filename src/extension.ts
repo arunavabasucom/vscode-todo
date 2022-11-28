f74984978906c45ed280d1bb79279ba53e172524
Arunava Basu <arunavabasuDev@gmail.com>
2022-11-29 01:14:26 +0530

    adding basic webviews and setting up rollup

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,49 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+//www.youtube.com/watch?v=a5DX5pQ9p5M&t=3632s
+import * as vscode from "vscode";
+import { HelloWorldPanel } from "./HelloWorldPanel";
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+  console.log('Congratulations, your extension "vscode-todo" is now active!');
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "vscode-todo" is now active!');
+  // registering a command
+  // context.subscriptions.push(
+  //   vscode.commands.registerCommand("vscode-todo.helloWorld", () => {
+  //     vscode.window.showInformationMessage("Hello World from vscode_todo!");
+  //   })
+  // );
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('vscode-todo.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from vscode_todo!');
-	});
+  context.subscriptions.push(
+    vscode.commands.registerCommand("vscode-todo.refreshWebviews", () => {
+      HelloWorldPanel.kill();
+      HelloWorldPanel.createOrShow(context.extensionUri);
+      setTimeout(() => {
+        vscode.commands.executeCommand(
+          " workbench.action.webview.openDeveloperTools"
+        );
+      }, 500);
+    })
+  );
+  context.subscriptions.push(
+    vscode.commands.registerCommand("vscode-todo.helloWorld", () => {
+      /* webview panel >> view */
+      HelloWorldPanel.createOrShow(context.extensionUri);
+    })
+  );
 
-	context.subscriptions.push(disposable);
+  context.subscriptions.push(
+    vscode.commands.registerCommand("vscode-todo.askQuestion", async () => {
+      const answer = await vscode.window.showInformationMessage(
+        "What is favorite animal ?",
+        "good",
+        "bad"
+      );
+      if (answer === "bad") {
+        vscode.window.showInformationMessage("Sorry to hear that !! ");
+      } else {
+        console.log({ answer });
+      }
+    })
+  );
 }
 
-// This method is called when your extension is deactivated
 export function deactivate() {}
---
+++ webviews/components/HelloWorld.svelte
@@ -0,0 +1,5 @@
+<!-- https://stackoverflow.com/questions/64194571/cannot-find-module-app-svelte-or-its-corresponding-type-declarations -->
+
+<Script lang="ts" />
+
+<h1>Hello from svelte new !!!!</h1>
